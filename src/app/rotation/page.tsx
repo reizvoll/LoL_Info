e11ef42601d8d0d6ebc493f7ec5cb1c7683d36c3
지영 <reizvoll@jiyeong-ui-MacBookAir.local>
2024-12-17 17:41:32 +0900

    fix: rotation page, provider 분리

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-import PageComponents from "./PageComponents";
+import PageComponents from "../../ui/rotation/PageComponents";
+import AppProvider from "./Provider";
 
 export default function RotationPage() {
-  const [queryClient] = useState(() => new QueryClient());
-
-  // 이거 구분지어야 합니둥 ㅇㅅㅇ. 여튼 그렇다구요 ㅇㅅㅇ..
   return (
-    <QueryClientProvider client={queryClient}>
+    <AppProvider>
       <PageComponents />
-    </QueryClientProvider>
+    </AppProvider>
   );
 }
