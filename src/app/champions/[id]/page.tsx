fec7e4459ebe125cc9c020c275f8ffd3faa6f916
지영 <reizvoll@jiyeong-ui-MacBookAir.local>
2024-12-18 11:27:22 +0900

    design: 챔피언 디테일 페이지 CSS 디자인 수정

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 const IMAGE_BASE_URL = "https://ddragon.leagueoflegends.com/cdn";
 const SPELL_IMAGE_BASE_URL = `${IMAGE_BASE_URL}/14.24.1/img/spell`;
 const PASSIVE_IMAGE_BASE_URL = `${IMAGE_BASE_URL}/14.24.1/img/passive`;
-const SKIN_IMAGE_BASE_URL = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash";
+const BACKGROUND_URL =
+  "https://ddragon.leagueoflegends.com/cdn/img/champion/splash";
 
 function parseTooltip(tooltip: string): string {
   let cleanTooltip = tooltip.replace(/<\/?[^>]+(>|$)|[^ ]+/g, "");
@@ -39,48 +40,58 @@ export default async function ChampionDetailPage({
   params,
 }: ChampionDetailPageProps) {
   const champion: ChampionDetail = await fetchChampionDetail(params.id);
-  const version = await fetchLatestVersion();
 
   return (
-    <div style={{ padding: "20px", maxWidth: "300px", margin: "0 auto" }}>
-      <h1>{champion.name}</h1>
-      <h2>{champion.title}</h2>
-      <Image
-        src={`${IMAGE_BASE_URL}/${version}/img/champion/${champion.image.full}`}
-        alt={champion.name}
-        width={800}
-        height={400}
-        style={{ borderRadius: "8px" }}
-      />
-      <p style={{ marginTop: "20px", fontSize: "1.1rem" }}>{champion.blurb}</p>
-
-      <div style={{ marginTop: "30px" }}>
-        <h3>Tags</h3>
-        <ul style={{ display: "flex", gap: "10px" }}>
-          {champion.tags.map((tag) => (
-            <li
-              key={tag}
-              style={{
-                backgroundColor: "#333",
-                padding: "5px 10px",
-                borderRadius: "5px",
-              }}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
+    <div className="relative w-full min-h-screen">
+      <div className="fixed top-0 left-0 w-full h-screen -z-10">
+        <Image
+          src={`${BACKGROUND_URL}/${champion.id}_0.jpg`}
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="top center"
+          priority
+          className="brightness-50 ]"
+        />
       </div>
 
-      <div style={{ marginTop: "30px" }}>
+      {/* 이름, 별칭(?), 스토리 섹션 */}
+      <div className="relative px-6 py-10 md:px-12 md:py-20 flex flex-col gap-8 text-white">
+        <div className="max-w-[45%]">
+          <h1 className="text-5xl md:text-6xl font-bold mb-4">
+            {champion.name}
+          </h1>
+          <h2 className="text-2xl md:text-3xl text-gray-300 mb-6">
+            {champion.title}
+          </h2>
+          <p className="text-lg leading-relaxed text-gray-300 hidden sm:block">
+            {champion.blurb}
+          </p>
+        </div>
+
+        {/* 태그 섹션 */}
+        <div className="flex gap-4">
+        <h3 className="text-2xl font-semibold mb-4"> 역할군 </h3>
+          {champion.tags.map((tag) => (
+            <span
+              key={tag}
+              className="px-4 py-2 bg-gray-800 bg-opacity-70 rounded-md text-white text-lg"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+
+        {/* 패시브 섹션 */}
         <div>
-          <h3>패시브</h3>
-          <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+          <h3 className="text-2xl font-semibold mb-4">패시브</h3>
+          <div className="flex items-center gap-4">
             <Image
               src={`${PASSIVE_IMAGE_BASE_URL}/${champion.passive.image.full}`}
               alt={champion.passive.name}
               width={64}
               height={64}
+              className="min-w-[64px] min-h-[64px]"
             />
             <div>
               <strong>{champion.passive.name}</strong>
@@ -89,67 +100,77 @@ export default async function ChampionDetailPage({
           </div>
         </div>
 
-        <div style={{ marginTop: "30px" }}>
-          <h3>스킬 정보</h3>
+        {/* 스킬 정보 */}
+        <div>
+          <h3 className="text-2xl font-semibold mb-4">스킬 정보</h3>
           {champion.spells.map((spell) => (
-            <div key={spell.id} style={{ marginTop: "20px" }}>
-              <h4>{spell.name}</h4>
-              <div
-                style={{ display: "flex", gap: "10px", alignItems: "center" }}
-              >
-                <Image
-                  src={`${SPELL_IMAGE_BASE_URL}/${spell.image.full}`}
-                  alt={spell.name}
-                  width={64}
-                  height={64}
-                />
-                <div>
-                  <p>{spell.description}</p>
-                  <p>
-                    <em>
-                      {spell.tooltip
-                        ? parseTooltip(spell.tooltip)
-                        : "해당 내용에 대한 정보가 없습니다."}
-                    </em>
-                  </p>
-                </div>
+            <div key={spell.id} className="mt-6 flex items-center gap-4">
+              <Image
+                src={`${SPELL_IMAGE_BASE_URL}/${spell.image.full}`}
+                alt={spell.name}
+                width={64}
+                height={64}
+                className="min-w-[64px] min-h-[64px]"
+              />
+              <div>
+                <strong>{spell.name}</strong>
+                <p>{spell.description}</p>
+                <p className="italic">
+                  {spell.tooltip
+                    ? parseTooltip(spell.tooltip)
+                    : "해당 내용에 대한 정보가 없습니다."}
+                </p>
               </div>
             </div>
           ))}
         </div>
-      </div>
 
-      <div style={{ marginTop: "30px" }}>
-        <h3>Tips for Allies</h3>
-        <ul style={{ paddingLeft: "20px" }}>
-          {champion.allytips.map((tip, index) => (
-            <li key={index}>{tip}</li>
-          ))}
-        </ul>
-      </div>
-
-      <div style={{ marginTop: "30px" }}>
-        <h3>Tips for Enemies</h3>
-        <ul style={{ paddingLeft: "20px" }}>
-          {champion.enemytips.map((tip, index) => (
-            <li key={index}>{tip}</li>
-          ))}
-        </ul>
-      </div>
-      <h2>Skins</h2>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-        {champion.skins.map((skin) => (
-          <div key={skin.id} style={{ textAlign: "center" }}>
-            <Image
-              src={`${SKIN_IMAGE_BASE_URL}/${champion.id}_${skin.num}.jpg`}
-              alt={skin.name}
-              width={300}
-              height={500}
-              style={{ borderRadius: "8px" }}
-            />
-            <p>{skin.name}</p>
+        {/* 아군을 위한 팁 섹션 */}
+        {champion.allytips?.length > 0 && (
+          <div>
+            <h3 className="text-2xl font-semibold mb-4">아군을 위한 Tip</h3>
+            <ul className="list-disc list-inside space-y-2">
+              {champion.allytips.map((tip, index) => (
+                <li key={index} className="text-gray-300">
+                  {tip}
+                </li>
+              ))}
+            </ul>
           </div>
-        ))}
+        )}
+
+        {/* 적군을 위한 팁 섹션 */}
+        {champion.enemytips?.length > 0 && (
+          <div>
+            <h3 className="text-2xl font-semibold mb-4">적군을 위한 Tip</h3>
+            <ul className="list-disc list-inside space-y-2">
+              {champion.enemytips.map((tip, index) => (
+                <li key={index} className="text-gray-300">
+                  {tip}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {/* Skins 섹션 */}
+        <div>
+          <h3 className="text-2xl font-semibold mb-4">Skins</h3>
+          <div className="flex flex-wrap gap-6">
+            {champion.skins.map((skin) => (
+              <div key={skin.id} className="text-center">
+                <Image
+                  src={`${BACKGROUND_URL}/${champion.id}_${skin.num}.jpg`}
+                  alt={skin.name}
+                  width={200}
+                  height={450}
+                  className="rounded-md"
+                />
+                <p>{skin.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
